63f9550ce24209321289c236244bc57ebe814829
daichi-m <majumdarsohom@gmail.com>
2020-04-10 10:14:57 +0530

    Packaging changes

--- main/togo.go
+++ main/togo.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"github.com/daichi-m/togo"
-	"log"
-	"os"
 )
 
 func main() {
-	//fmt.Println("vim-go")
-	f, err := os.Open("../samples/sample-map.json")
-	if err != nil {
-		log.Fatal("Error in opening file", err)
-		os.Exit(1)
-	}
-	togo.Decode(f)
+	js := new(togo.Json)
+	js.File = "../samples/sample-slice.json"
+	togo.Parse(js)
 }
--- types.go
+++ types.go
@@ -1,22 +1,28 @@
 package togo
 
-import "io"
-
+/* The struct that donates a go field inside the go struct that will be generated.
+ * It has a Name, a Type and optionally an Annotation to user for (un)marshalling
+ */
 type Field struct {
-	Name string,
-	Type string,
-	Annotation string
+	Name, Type, Annotation string
 }
 
+/* The representation of a go struct. It has a name, a set of Field types and a Level
+ * to determine at what level should the struct be defined in the final source code.
+ */
 type GoStruct struct {
-	Name string,
-	Fields []Field,
-	Level int
+	Name   string
+	Fields []Field
+	Level  int
 }
 
-type GoStructer interface {
-	HandleMap(mp map[string]interface{}) (GoStruct, error)
-	HandleSlice(sl []interface{}) (GoStruct, error)
-	Decode(f io.Reader) (error)
-	ToGoStruct() (string, error)
+/* The decoded data represented as a struct */
+type DecData struct {
+	MapData   map[string]interface{}
+	SliceData []interface{}
+}
+
+/* Interface type that defines decodability for the parser to process */
+type Decoder interface {
+	Decode() (DecData, error)
 }
