a63ab8305021bec37f55e9e5d81f2db2cb786292
daichi-m <majumdarsohom@gmail.com>
2020-04-10 16:27:27 +0530

    Few more parser changes

--- parser.go
+++ parser.go
@@ -2,6 +2,7 @@ package togo
 
 import (
 	"log"
+	"reflect"
 )
 
 func Parse(dec Decoder) error {
@@ -11,24 +12,71 @@ func Parse(dec Decoder) error {
 		log.Println("Error while decoding data", err)
 		return err
 	}
-	if data.MapData != nil {
-		HandleMap(data.MapData)
-	} else {
-		HandleSlice(data.SliceData)
+	mp, err := data.ToMap()
+	if err == nil {
+		handleMap(mp)
+	}
+	sl, err := data.ToSlice()
+	if err == nil {
+		handleSlice(sl)
 	}
 	return nil
 }
 
-func HandleMap(m map[string]interface{}) error {
+func handleMap(m map[string]interface{}) (gs GoStruct, err error) {
+
+	var set bool
+	var gs GoStruct
+	var fields []Field
 	for k, v := range m {
-		log.Printf("%s -> %v.(%T)\n", k, v, v)
+		set = false
+		fld := new(Field)
+		fld.Name = k
+		switch v.(type) {
+		case int:
+			fld.Type = Int
+			set = true
+		case int64:
+			fld.Type = BigInt
+			set = true
+		case string:
+			fld.Type = String
+			set = true
+		case float32:
+			fld.Type = Float32
+			set = true
+		case float64:
+			fld.Type = Float64
+			set = true
+		default:
+			set = false
+		}
+
+		if !set {
+			tp := reflect.ValueOf(v).Kind()
+			switch tp {
+			case reflect.Slice:
+				sl := v.([]interface{})
+				mgs, err := handleSlice(sl)
+				if err != nil {
+					return gs, err
+				}
+			case reflect.Map:
+				mp := v.(map[string]interface{})
+				sgs, err := handleMap(mp)
+				if err != nil {
+					return gs, err
+				}
+			}
+		}
+		fields = append(fields, *fld)
 	}
-	return nil
+	gs.Fields = fields
+	return gs, nil
 }
 
-func HandleSlice(s []interface{}) error {
+func handleSlice(s []interface{}) (gs GoStruct, err error) {
 	for i, v := range s {
 		log.Printf("%d -> %v.(%T)\n", i, v, v)
 	}
-	return nil
 }
--- types.go
+++ types.go
@@ -1,10 +1,13 @@
 package togo
 
+import "errors"
+
 /* The struct that donates a go field inside the go struct that will be generated.
  * It has a Name, a Type and optionally an Annotation to user for (un)marshalling
  */
 type Field struct {
-	Name, Type, Annotation string
+	Name, Annotation string
+	Type             FieldType
 }
 
 /* The representation of a go struct. It has a name, a set of Field types and a Level
@@ -16,13 +19,48 @@ type GoStruct struct {
 	Level  int
 }
 
+type FieldType uint
+
+const (
+	Int = iota
+	BigInt
+	Float32
+	Float64
+	String
+	Slice
+	Map
+)
+
 /* The decoded data represented as a struct */
 type DecData struct {
-	MapData   map[string]interface{}
-	SliceData []interface{}
+	mapData   map[string]interface{}
+	sliceData []interface{}
+}
+
+func (dd *DecData) OfMap(mp map[string]interface{}) {
+	dd.mapData = mp
+}
+
+func (dd *DecData) OfSlice(sl []interface{}) {
+	dd.sliceData = sl
+}
+
+func (dd DecData) ToMap() (map[string]interface{}, error) {
+	if dd.mapData == nil {
+		return nil, errors.New("MapData is not set")
+	}
+	return dd.mapData, nil
+}
+
+func (dd DecData) ToSlice() ([]interface{}, error) {
+	if dd.sliceData == nil {
+		return nil, errors.New("SliceData is not set")
+	}
+	return dd.sliceData, nil
 }
 
 /* Interface type that defines decodability for the parser to process */
 type Decoder interface {
 	Decode() (DecData, error)
+	Annotation() string
 }
