f07b58e05f64e42e5c8fbf2228365854ba3eca7f
daichi-m <majumdarsohom@gmail.com>
2020-04-10 16:27:27 +0530

    Few more parser changes

--- json_decoder.go
+++ json_decoder.go
@@ -14,32 +14,30 @@ type Json struct {
 
 func (j *Json) Decode() (DecData, error) {
 
-	var dd DecData
+	dd := new(DecData)
 	f, err := os.Open(j.File)
 	if err != nil {
 		log.Println("Error while reading file", err)
-		return dd, err
+		return *dd, err
 	}
 	var val interface{}
 	dec := json.NewDecoder(f)
 	err = dec.Decode(&val)
 	if err != nil {
 		log.Println("Error while decoding", err)
-		return dd, err
+		return *dd, err
 	}
 	tp := reflect.ValueOf(val)
 	switch tp.Kind() {
 	case reflect.Map:
 		mp := val.(map[string]interface{})
-		dd.MapData = mp
-		dd.SliceData = nil
+		dd.OfMap(mp)
 	case reflect.Slice:
 		sl := val.([]interface{})
-		dd.MapData = nil
-		dd.SliceData = sl
+		dd.OfSlice(sl)
 	default:
 		log.Println("Unknown type to decode", tp.Kind())
-		return dd, errors.New("Unknown type to decode")
+		return *dd, errors.New("Unknown type to decode")
 	}
-	return dd, nil
+	return *dd, nil
 }
