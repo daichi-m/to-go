05a22f344d795a2d070489d8dab6d45b08f1eb61
daichi-m <majumdarsohom@gmail.com>
2020-05-27 20:55:44 +0530

    Added new errors

--- errors.go
+++ errors.go
@@ -1,6 +1,9 @@
 package togo
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // CacheAddError is an error type describing an error while adding an element to a cache
 type CacheAddError struct {
@@ -33,4 +36,35 @@ type NameConflictError struct {
 func (nce NameConflictError) Error() string {
 	return fmt.Sprintf("Name %s is already taken and cannot successfully create a new name",
 		nce.name)
+}
+
+// FieldError indicates that the Field operation has failed due to some issue.
+type FieldError struct {
+	field   Field
+	message string
+}
+
+func (fe FieldError) Error() string {
+	return fmt.Sprintf("Field %s errored due to %s", fe.field.name, fe.message)
+}
+
+// UnsupportedType is an error type whenever an unsupported data type is encountered
+// during creation of a Field or GoStruct
+type UnsupportedType struct {
+	data interface{}
+}
+
+func (ut UnsupportedType) Error() string {
+	tp := reflect.TypeOf(ut.data).Kind()
+	return fmt.Sprintf("Data type %s is not recognized", tp)
+}
+
+// GoStructError indicates that operation on the GoStruct failed due to some issue.
+type GoStructError struct {
+	gs      GoStruct
+	message string
+}
+
+func (gse GoStructError) Error() string {
+	return fmt.Sprintf("Field %s errored due to %s", gse.gs.Name, gse.message)
 }
